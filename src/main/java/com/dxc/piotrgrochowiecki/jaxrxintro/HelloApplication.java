fb3e16443aad9fc8774e41ee0a9a4b689ebbf77e
Grochowiecki <piotr.grochowiecki@dxc.com>
2023-03-20 16:27:19 +0100

    Initial commit

---
+++ src/main/java/com/dxc/piotrgrochowiecki/jaxrxintro/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.dxc.piotrgrochowiecki.jaxrxintro;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/dxc/piotrgrochowiecki/jaxrxintro/HelloResource.java
@@ -0,0 +1,14 @@
+package com.dxc.piotrgrochowiecki.jaxrxintro;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
